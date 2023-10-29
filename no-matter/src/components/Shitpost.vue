f5e217e877743e592e66f45808043325294759ae
Kostya <ckutls228@gmail.com>
2023-10-29 18:41:27 +0300

    feat: add file for header on all pages and create shtpst component

---
+++ no-matter/src/components/Shitpost.vue
@@ -0,0 +1,13 @@
+<template>
+  <header-element></header-element>
+</template>
+
+<script>
+import headerElement from "@/pages/headerElement.vue";
+
+export default {
+  name: 'ShitPostComponent',
+  components: { headerElement }
+}
+
+</script>
---
+++ no-matter/src/pages/headerElement.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div class="header__logo">
+        <h1>No Matter</h1>
+      </div>
+      <h1>You Shit Posts</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'headerElement'
+}
+</script>
