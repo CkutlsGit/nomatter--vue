6706aab26781c0868a054e0947925ab0aa464743
Kostya <ckutls228@gmail.com>
2023-10-29 18:19:27 +0300

    style: фadd template for mainpage

---
+++ no-matter/src/pages/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+        <div class="header__content">
+            <div class="header__logo">
+                No Matter 
+            </div>
+            <h1>You Shit Posts</h1>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainPage'
+}
+</script>
+
+<style>
+
+</style>
