ea4b72a0342679ffe7172492fb1e2906c1ee4c5c
Kostya <ckutls228@gmail.com>
2023-10-30 14:14:07 +0300

    style: add normal template for modal

--- no-matter/src/components/Modal.vue
+++ no-matter/src/components/Modal.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="dialog">
-    <h1>HELLO</h1>
-    <h1>{{ nickname }}</h1>
-    <h1>{{ title }}</h1>
-    <h1>{{ description }}</h1>
-    <input type="text" v-model.trim="nickname">
-    <input type="text" v-model.trim="description">
-    <input type="text" v-model.trim="title">
-    <button @click="sendValue">Send</button>
-    <button @click="closeModal">Close</button>
+    <div class="dialog__content">
+      <h1 class="dialog__content--title">Напишите пост</h1>
+      <div class="dialog__inputs">
+        <input type="text" v-model.trim="nickname">
+        <input type="text" v-model.trim="description">
+        <input type="text" v-model.trim="title">
+      </div>
+      <div class="dialog__btns">
+        <button @click="sendValue">Send</button>
+        <button @click="closeModal">Close</button>
+      </div>
+    </div>
   </div>
 </template>
 
