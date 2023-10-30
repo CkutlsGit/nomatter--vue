242e5e940fa3164c43fd3f763a3c1bee65bb4115
Kostya <ckutls228@gmail.com>
2023-10-30 09:07:20 +0300

    feat: add file modal and logic for his

---
+++ no-matter/src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dialog">
+    <dialog>
+      <h1>HELLO</h1>
+      <button @click="closeModal">Close</button>
+    </dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
