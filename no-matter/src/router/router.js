dc1a3c9c87dd31f94c6419cbb02cc7f4d9ed5e38
Kostya <ckutls228@gmail.com>
2023-10-30 08:50:00 +0300

    feat: add path for shitpost

--- no-matter/src/router/router.js
+++ no-matter/src/router/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Import pages:
 import mainPage from "@/pages/MainPage.vue";
+import Shitpost from "@/components/Shitpost.vue";
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: mainPage }
+        { path: '/', component: mainPage },
+        { path: '/shtpst', component:  Shitpost}
     ]
 })
