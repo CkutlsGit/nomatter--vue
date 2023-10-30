6c9919d4020026a4fdc10aa30e38355146092f0b
Kostya <ckutls228@gmail.com>
2023-10-30 09:07:01 +0300

    feat: add logic by clicked on btn and call modal window

--- no-matter/src/components/Shitpost.vue
+++ no-matter/src/components/Shitpost.vue
@@ -1,13 +1,30 @@
 <template>
   <header-element></header-element>
+  <section class="write">
+    <div class="write__content">
+      <button @click="isOpen = true">Написать</button>
+      <modal
+          v-if="isOpen"
+          @close="isOpen = false"
+      >
+      </modal>
+    </div>
+  </section>
 </template>
 
 <script>
 import headerElement from "@/pages/headerElement.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: 'ShitPostComponent',
-  components: { headerElement }
+  components: { headerElement, Modal },
+
+  data() {
+    return {
+      isOpen: false
+    }
+  },
 }
 
 </script>
