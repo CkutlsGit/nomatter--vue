8de3eca51a20b27876028a0eb07366c8b3c9c39b
Kostya <ckutls228@gmail.com>
2023-10-30 09:16:13 +0300

    feat: add logic for send value-title

--- no-matter/src/components/Modal.vue
+++ no-matter/src/components/Modal.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="dialog">
     <h1>HELLO</h1>
+    <h1>{{ title }}</h1>
+    <input type="text" v-model.trim="title">
+    <button @click="sendValue">Send</button>
     <button @click="closeModal">Close</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '',
+    }
+  },
   methods: {
     closeModal() {
       this.$emit('close')
+    },
+    sendValue() {
+      this.$emit('send', this.title)
     }
   }
 }
