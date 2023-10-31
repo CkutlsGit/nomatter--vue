341557414e6eb039adbdf995b231a16d004564ad
Kostya <ckutls228@gmail.com>
2023-10-31 12:55:54 +0300

    style: add for main page

--- no-matter/src/pages/MainPage.vue
+++ no-matter/src/pages/MainPage.vue
@@ -21,6 +21,34 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.start-work {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+.start-work__content {
+  margin-top: 150px;
+  border-radius: 5px;
+  width: 800px;
+  height: 400px;
+  background: #fff;
+}
+.start-work__content h2 {
+  font-size: 20px;
+  padding: 60px 20px;
+}
+.start__work__btn a {
+  text-decoration: none;
+  color: #fff;
+  background: #439a2f;
+  padding: 15px 40px;
+  font-weight: 700;
+  font-size: 29px;
+  transition: .7s ease;
+}
+.start__work__btn a:hover {
+  background: #d5d1b9;
+  color: #000000;
+}
 </style>
